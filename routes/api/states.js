802c4e5f4be2d2e301d6b80477949b855c204ac2
sephbarnes <e.josephbarnes@gmail.com>
2023-05-10 22:57:17 -0500

    Update states.js

--- routes/api/states.js
+++ routes/api/states.js
@@ -4,14 +4,22 @@ const statesController = require("../../controllers/statesController");
 
 router.route("/").get(statesController.getAllStates);
 router.route("/:code").get(statesController.getState);
-router.route("/:code/funfact").get(statesController.getFunFact);
+router.route("/:code/funfact")
+  .get(statesController.getFunFact)
+  .post(statesController.postFunFacts)
+  .patch(statesController.patchFunFacts)
+  .delete(statesController.deleteFunFact);
+
 router.route("/:code/capital").get(statesController.getCapital);
+router.route("/:code/nickname").get(statesController.getNickname);
+router.route("/:code/population").get(statesController.getPopulation);
+router.route("/:code/admission").get(statesController.getAdmission);
 
 
-router.route("/:code/funfact").post(statesController.postFunFacts);
+//router.route("/:code/funfact").post(statesController.postFunFacts);
 
-router.route("/:code/funfact").patch(statesController.patchFunFacts);
-router.route("/:code/funfact").delete(statesController.deleteFunFact);
+//router.route("/:code/funfact").patch(statesController.patchFunFacts);
+//router.route("/:code/funfact").delete(statesController.deleteFunFact);
 
 //router.route("/:id")
 //.get(statesController.getStateFacts);
