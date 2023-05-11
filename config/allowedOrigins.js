5e0f06c20eb34f0ad27602a1c0e92cd3111abe95
sephbarnes <e.josephbarnes@gmail.com>
2023-05-10 22:59:32 -0500

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-    'https://www.yoursite.com',
+    'https://suave-mini-recorder.glitch.me/',    
+    'https://suave-mini-recorder.glitch.me',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
